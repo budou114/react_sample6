095f36024ba2923ec8d3291ef2d07f5ea7f5c2db
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-23 08:55:32 +0900

    取得データの型を定義しバグを防ぐ

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import { useState } from 'react';
 import './App.css';
 import { Todo } from './Todo';
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => {
+    axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
+    .then((res) => {
+      res.data.map((todo) => todo.title);
       setTodos(res.data);
     });
   };
@@ -16,7 +25,7 @@ export default function App() {
     <div className='App'>
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.title} />
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
