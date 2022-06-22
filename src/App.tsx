8d31e9cd33b291d04bda294f84dbd2d92751ed57
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-23 08:33:29 +0900

    事前準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App() {
+  const onClickFetchData = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
+      <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
-}
-
-export default App;
+};
